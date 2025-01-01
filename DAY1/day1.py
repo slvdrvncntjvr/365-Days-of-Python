e878e74c90131e5ab8fb038695b4f3804e4ca4af
Salvador Vincent Javier <slvdrvncntjvr@gmail.com>
2025-01-01 20:41:22 +0800

    Initial commit: Starting 365 Days of Python

---
+++ DAY1/day1.py
@@ -0,0 +1,9 @@
+from pyfiglet import figlet_format
+
+# Install pyfiglet using: pip install pyfiglet
+def main():
+    banner = figlet_format("Hello, World!")
+    print(banner)
+
+if __name__ == "__main__":
+    main()
