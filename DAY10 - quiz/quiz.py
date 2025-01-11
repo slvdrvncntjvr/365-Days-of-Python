f51ce987c0e54de2325f153d5174fe39f7f304aa
Salvador Vincent Javier <slvdrvncntjvr@gmail.com>
2025-01-11 20:00:39 +0800

    separated the questions.json of quiz

--- DAY10 - quiz/quiz.py
+++ DAY10 - quiz/quiz.py
@@ -1,11 +1,9 @@
+import json
+
 class QuestionBank:
-    def __init__(self):
-      #insert questions here
-        self.questions = [
-            {"question": "What is the capital of France?", "options": ["Paris", "London", "Rome", "Berlin"], "answer": "Paris"},
-            {"question": "What is 2 + 2?", "options": ["3", "4", "5", "6"], "answer": "4"},
-            {"question": "Who wrote '1984'?", "options": ["George Orwell", "J.K. Rowling", "Ernest Hemingway", "Jane Austen"], "answer": "George Orwell"}
-        ]
+    def __init__(self, json_file='questions.json'):
+        with open (json_file, 'r') as file:
+            self.questions = json.load(file)
         self.current_index = 0
 
     def get_current_question(self):
@@ -14,3 +12,5 @@ class QuestionBank:
     def next_question(self):
         self.current_index += 1
         return self.current_index < len(self.questions)
+
+        
